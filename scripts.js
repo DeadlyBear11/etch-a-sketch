fa2458deff6dd0205b7e6099650b3e541792391e
guijeco <86424543+DeadlyBear11@users.noreply.github.com>
2022-11-12 09:11:34 -0700

    Created button to change the num of blocks

--- scripts.js
+++ scripts.js
@@ -16,9 +16,8 @@ function changeColor(e) {
 // Function to add event-listener to every block.
 function addEventToChildren(element) {
     const children = element.childNodes;
-    const arr = [...children];
-    for (let j = 0; j < 256; j++) {
-        let block = arr[j];
+    for (let j = 0; j < children.length; j++) {
+        const block = children[j];
         block.addEventListener("mouseover", changeColor);
     }
 }
@@ -26,3 +25,32 @@ function addEventToChildren(element) {
 // Add event-listener with changeColor to each children of container.
 addEventToChildren(container);
 
+// Function to change the number of blocks inside container.
+// Ask the user to input the number of blocks per side desired (max. 100).
+const btn = document.querySelector("#button");
+btn.addEventListener("click", changeBlocksNum);
+
+function changeBlocksNum() {
+    let nBlocks = 0;
+
+    // Deal with numbers higher than 100.
+    do {
+    nBlocks = +prompt("Enter the number of desired blocks per side (max. 100):");
+    } while (nBlocks > 100);
+
+    let blockSize = 512 / nBlocks;
+
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+
+    // Build the new canvas of blocks.
+    for (let k = 1; k <= (nBlocks**2); k++) {
+        const block = document.createElement("div");
+        block.style.width = `${blockSize}px`;
+        block.style.height = `${blockSize}px`;
+        block.classList.add("block-new");
+        block.addEventListener("mouseover", () => block.classList.add("hover-color"));
+        container.appendChild(block);
+    }
+}
